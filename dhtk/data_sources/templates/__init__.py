a98e7920ed476e217b328636a4ac97c996306472
yael.iseli <yael.iseli@unil.ch>
2024-07-08 16:13:06 +0200

    Update github action

--- dhtk/data_sources/templates/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-"""
-Common modules of dhtk.
-
-Contains abstract classes and corpus class which are used by submodules.
-"""
--- dhtk/storage/docker/sql/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-"""
-This module contains sql databases as submodules.
-
-DEFAULT selects the name of the default module to use.
-"""
-# noinspection PyUnusedName
-DEFAULT: str = "mariadb"
